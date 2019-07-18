fe72a15cc9b97a7cc3c6bbd2c5394a8f46bf89f6
Alex Vakhtin <32684110+elementalmagic@users.noreply.github.com>
2019-07-18 12:59:08 +0400

    modal++

--- fondsberejeniy/js/main.js
+++ fondsberejeniy/js/main.js
@@ -89,7 +89,7 @@ $(function () {
         var result = amount * (0.18 / 12 * $('#period').val());
         $('#res')[0].innerText = Math.floor(result).toLocaleString();
     })
-})
+});
 
 function initialCalc() {
     var slider = $('#slider')[0];
@@ -116,4 +116,111 @@ function initialCalc() {
     var result = value * (0.18 / 12 * $('#period').val());
     amount = value;
     $('#res')[0].innerText = Math.floor(result).toLocaleString();
+}
+
+var modal = {
+    el: $('#modal')[0],
+    formElems: [],
+    phoneInputField: '',
+    phoneMaskConfig: {mask: '+{7} (000) 000-00-00'},
+    phoneMask: null,
+    checkValid: function () {
+        let valid = true;
+        if ($('#name')[0].value < 1) valid = false;
+        if ($('#phone')[0].value < 18) valid = false;
+        return valid
+    },
+};
+$(function () {
+    initButtons();
+    initModal();
+});
+
+function initButtons() {
+    var butArr = $('.modal-button-open');
+    console.log(butArr);
+    [].forEach.call(butArr, function (button) {
+        button.addEventListener('click', function () {
+            modal.el.classList.add('is-active');
+        })
+    });
+
+    $('.modal-close').on('click', function () {
+        modal.el.classList.remove('is-active');
+    });
+    $('#button_ok').on('click', function () {
+        modal.el.classList.remove('is-active');
+    });
+    $('#submit').on('click', function () {
+        let body = {
+            name: $('#name')[0].value,
+            phone: $('#phone')[0].value,
+            request_key: 'nhbnjy'
+        };
+        $('#submit').attr('disabled', true);
+        $('#name').attr('disabled', true);
+        $('#phone').attr('disabled', true);
+
+        axios.post('http://localhost/api/order/mail', body)
+            .then(res => {
+                $('#submit')[0].style = 'display: none';
+                $('#button_ok')[0].style = '';
+            })
+            .catch(err => {
+                $('#submit')[0].innerText = 'Произошла ошибка, попробуйте позже!';
+
+            })
+            .finally(() => {
+                $('#submit').attr('disabled', false);
+                $('#name').attr('disabled', false);
+                $('#phone').attr('disabled', false);
+            })
+    })
+}
+
+function initModal() {
+    modal.formElems = [
+        $('#name')[0],
+        $('#phone')[0]
+    ];
+    modal.phoneInputField = $('#phone')[0];
+    modal.phoneMask = new IMask(modal.phoneInputField, modal.phoneMaskConfig);
+    modal.phoneMask.value = '7';
+
+    for (let i = 0; i < modal.formElems.length; i++) {
+        const formElem = modal.formElems[i];
+
+        formElem.addEventListener('blur', function (event) {
+            if (!event.target.checkValidity()) {
+                event.target.style = 'border: 1px solid red'
+            } else {
+                event.target.style = 'border: 1px solid #9e9e9e'
+            }
+        });
+        formElem.addEventListener('input', function (event) {
+            if (!modal.checkValid()) {
+                $('#submit')[0].setAttribute("disabled", "disabled");
+            } else {
+                $('#submit')[0].removeAttribute("disabled");
+            }
+
+            if (event.target.id !== 'phone') {
+                if (event.target.checkValidity()) {
+                    event.target.style = 'border: 1px solid #9e9e9e'
+                }
+
+                if (event.target.value.length > 0) {
+                    event.target.classList.add('focus')
+                } else {
+                    event.target.classList.remove('focus')
+                }
+            } else {
+                if (event.target.value === '+7' || event.target.value === '') {
+                    this.phoneMask.value = '7'
+                }
+            }
+        })
+    }
+
+
 }
